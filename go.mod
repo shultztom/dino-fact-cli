58a5929b17fba8db6ba1bc103bea44d5f8e9bec2
shultztom <shultzthomask@sau.edu>
2022-05-06 15:26:57 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dino-cli
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Dino struct {
+	Name        string
+	Description string
+}
+
+func main() {
+	resp, err := http.Get("https://dino-facts-api.shultzlab.com/dinosaurs/random")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var dino Dino
+	err = json.Unmarshal(body, &dino)
+	if err != nil {
+		fmt.Printf("Error format Dino Fact!")
+		return
+	}
+	fmt.Printf("DINO FACT!! \nName: %s \nDescription: %s\n", dino.Name, dino.Description)
+	fmt.Printf("               __\n              / _)\n     _.----._/ /\n    /         /\n __/ (  | (  |\n/__.-'|_|--|_|")
+}
